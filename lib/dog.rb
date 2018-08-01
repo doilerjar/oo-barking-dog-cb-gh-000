ce3124ce7d6ca563f80040cb4e7a372e16d75586
Kevin K <doilerjar@outlook.com>
2018-08-01 07:43:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+	def initialize(name)
+		@name = name
+	end
+	
+	def name=(dogs_name)
+		@name = dogs_name
+	end
+	
+	def name   # getter
+		@name 
+	end
+	
+	def bark
+	  puts "woof!"
+	end 
+end
